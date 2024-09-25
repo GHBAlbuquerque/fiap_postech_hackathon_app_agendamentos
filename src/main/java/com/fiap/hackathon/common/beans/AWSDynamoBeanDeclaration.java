46df2796dd03d83fc1b8c0616ff14c443df67f10
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2024-09-24 22:13:41 -0300

    refactor: ajuste na geracao do bean do dynamo e remocao de bean nao utilizado

--- src/main/java/com/fiap/hackathon/common/beans/AWSDynamoBeanDeclaration.java
+++ src/main/java/com/fiap/hackathon/common/beans/AWSDynamoBeanDeclaration.java
@@ -1,7 +1,5 @@
 package com.fiap.hackathon.common.beans;
 
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.regions.Region;
@@ -11,14 +9,10 @@ import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 public class AWSDynamoBeanDeclaration {
 
     @Bean
-    public AmazonDynamoDB amazonDynamoDB() {
-        return AmazonDynamoDBClient.builder()
-                .withRegion(String.valueOf(Region.US_EAST_1))
-                .build();
-    }
-
-    @Bean
     public DynamoDbClient dynamoDbClient() {
-        return DynamoDbClient.create();
+        return DynamoDbClient
+                .builder()
+                .region(Region.US_EAST_1)
+                .build();
     }
 }
